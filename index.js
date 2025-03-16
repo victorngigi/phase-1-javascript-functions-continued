c4c658edd6947023d558fe91e3a78ce7a0e72831
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-03-16 17:39:48 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(flair="*"){
+    return function inner(param="special"){
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+console.log(saturdayFun("watch F1"));
+console.log(mondayWork("make 1 million chappatis"));
+console.log(wrapAdjective("*")("eating too much"));
+
